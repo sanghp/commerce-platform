57a74fe25f698740944185945ffea6cb8266cfab
sanghp <hoonprd@gmail.com>
2025-08-01 03:00:57 +0900

    refactor: Message Publisher 인터페이스에서 불필요한 콜백 파라미터 제거
    - BiConsumer<OutboxMessage, OutboxStatus> outboxCallback 파라미터 제거
    - 각 Publisher가 직접 OutboxHelper를 주입받아 처리하도록 변경

--- order-service/order-domain/order-application-service/src/main/java/com/commerce/platform/order/service/domain/ports/output/message/publisher/product/ProductReservationRequestMessagePublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/com/commerce/platform/order/service/domain/ports/output/message/publisher/product/ProductReservationRequestMessagePublisher.java
@@ -1,12 +1,8 @@
 package com.commerce.platform.order.service.domain.ports.output.message.publisher.product;
 
 import com.commerce.platform.order.service.domain.outbox.model.OrderOutboxMessage;
-import com.commerce.platform.outbox.OutboxStatus;
-
-import java.util.function.BiConsumer;
 
 public interface ProductReservationRequestMessagePublisher {
 
-    void publish(OrderOutboxMessage orderOutboxMessage,
-                 BiConsumer<OrderOutboxMessage, OutboxStatus> outboxCallback);
+    void publish(OrderOutboxMessage orderOutboxMessage);
 }
--- product-service/product-domain/product-application-service/src/main/java/com/commerce/platform/product/service/domain/ports/output/message/publisher/ProductReservationResponseMessagePublisher.java
+++ product-service/product-domain/product-application-service/src/main/java/com/commerce/platform/product/service/domain/ports/output/message/publisher/ProductReservationResponseMessagePublisher.java
@@ -1,14 +1,8 @@
 package com.commerce.platform.product.service.domain.ports.output.message.publisher;
 
 import com.commerce.platform.product.service.domain.outbox.model.ProductOutboxMessage;
-import com.commerce.platform.outbox.OutboxStatus;
-
-import java.util.function.BiConsumer;
 
 public interface ProductReservationResponseMessagePublisher {
 
-    default void publish(ProductOutboxMessage outboxMessage,
-                         BiConsumer<ProductOutboxMessage, OutboxStatus> outboxCallback) {
-
-    }
+    void publish(ProductOutboxMessage outboxMessage);
 } 
