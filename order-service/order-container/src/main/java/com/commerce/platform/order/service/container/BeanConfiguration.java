a6b188037466ce485684c2660c39e06bcb00f8f5
sanghp <hoonprd@gmail.com>
2025-08-14 03:04:59 +0900

    feat: Spring RestClient 적용 및 서비스 간 HTTP 통신 구현

--- order-service/order-container/src/main/java/com/commerce/platform/order/service/container/BeanConfiguration.java
+++ order-service/order-container/src/main/java/com/commerce/platform/order/service/container/BeanConfiguration.java
@@ -2,10 +2,14 @@ package com.commerce.platform.order.service.container;
 
 import com.commerce.platform.order.service.domain.OrderDomainService;
 import com.commerce.platform.order.service.domain.OrderDomainServiceImpl;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.core5.util.Timeout;
 
 import java.time.Duration;
 
@@ -18,11 +22,30 @@ public class BeanConfiguration {
     }
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .setConnectTimeout(Duration.ofSeconds(5))
-                .setReadTimeout(Duration.ofSeconds(10))
+    public RestClient restClient() {
+        var requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(2000))
+                .setResponseTimeout(Timeout.ofMilliseconds(5000))
+                .setConnectionKeepAlive(Timeout.ofSeconds(30))
+                .build();
+
+        var connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        var httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(connectionManager)
+                .evictIdleConnections(Timeout.ofSeconds(60))
+                .evictExpiredConnections()
+                .build();
+
+        var requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        return RestClient.builder()
+                .requestFactory(requestFactory)
                 .build();
     }
+
 }
 
--- payment-service/payment-container/src/main/java/com/commerce/platform/payment/service/container/PaymentServiceApplication.java
+++ payment-service/payment-container/src/main/java/com/commerce/platform/payment/service/container/PaymentServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaRepositories(basePackages = { "com.commerce.platform.payment.service.dataaccess", "com.commerce.platform.dataaccess" })
 @EntityScan(basePackages = { "com.commerce.platform.payment.service.dataaccess", "com.commerce.platform.dataaccess"})
