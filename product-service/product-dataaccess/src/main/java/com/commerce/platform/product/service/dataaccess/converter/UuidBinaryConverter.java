c3897a64d6fc89ce0c9b2ffb6c86dfb9c9220d91
sanghp <hoonprd@gmail.com>
2025-08-14 03:04:22 +0900

    feat: UUID Binary Converter 클래스 추가

---
+++ product-service/product-dataaccess/src/main/java/com/commerce/platform/product/service/dataaccess/converter/UuidBinaryConverter.java
@@ -0,0 +1,38 @@
+package com.commerce.platform.product.service.dataaccess.converter;
+
+import org.jooq.Converter;
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class UuidBinaryConverter implements Converter<byte[], UUID> {
+    
+    @Override
+    public UUID from(byte[] databaseObject) {
+        if (databaseObject == null || databaseObject.length != 16) {
+            return null;
+        }
+        ByteBuffer bb = ByteBuffer.wrap(databaseObject);
+        return new UUID(bb.getLong(), bb.getLong());
+    }
+
+    @Override
+    public byte[] to(UUID userObject) {
+        if (userObject == null) {
+            return null;
+        }
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(userObject.getMostSignificantBits());
+        bb.putLong(userObject.getLeastSignificantBits());
+        return bb.array();
+    }
+
+    @Override
+    public Class<byte[]> fromType() {
+        return byte[].class;
+    }
+
+    @Override
+    public Class<UUID> toType() {
+        return UUID.class;
+    }
+}
